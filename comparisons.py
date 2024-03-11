37ae6935254125063ab0f2d8af253bf562bd9217
Chris <96923618+sincerelychris@users.noreply.github.com>
2024-03-11 15:16:04 -0500

    Add files via upload
    info about comparisons

---
+++ comparisons.py
@@ -0,0 +1,9 @@
+#comparisons
+# 
+# EQUAL ->                          ==
+# NOT EQUAL ->                      != 
+# GREATER THAN ->                   >
+# LESS THAN ->                      <
+# GREATER THAN OR EQUAL ->          >=
+# LESS THAN OR EQUAL ->             <=
+# OBJECT IDENTITY ->                is
